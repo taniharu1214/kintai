bb48cc0afef7941abae83aaee01cf61721eb3875
taniharu1214 <107756296+taniharu1214@users.noreply.github.com>
2022-07-26 16:05:35 +0900

    Add files via upload

---
+++ push.js
@@ -0,0 +1,13 @@
+function player_hello() {
+    elm = document.createElement("audio");
+    elm.src = "hello.mp3";
+    elm.type = "mp3";
+    elm.play();
+    }
+
+    function player_bye() {
+        elm = document.createElement("audio");
+        elm.src = "bye.mp3";
+        elm.type = "mp3";
+        elm.play();
+        }
